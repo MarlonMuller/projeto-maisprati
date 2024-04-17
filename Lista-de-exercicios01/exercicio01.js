00134a3016bc80c6cef88960c873b9edc380283d
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-04-17 06:59:11 -0300

    Update 17.04.24

---
+++ Lista-de-exercicios01/exercicio01.js
@@ -0,0 +1,9 @@
+/* Escreva um algoritmo para ler uma temperatura em graus Celsius, calcular e escrever o valor correspondente em graus Fahrenheit.*/
+
+const prompt = require('prompt-sync')();
+
+let temperaturaGrauCelsius = prompt('Digite a temperatura em graus Celsius: ')
+
+let temperaturaGrausFahrenheit = temperaturaGrauCelsius * 1.8 + 32;
+
+console.log(`A temperatura ${temperaturaGrauCelsius} em Graus Celsius equivale a temperatura ${temperaturaGrausFahrenheit} em graus Fahrenheit`)
