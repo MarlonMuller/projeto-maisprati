59bf5bd9731b3cf535f9e0026e2040e548e976a6
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-04-18 09:21:58 -0300

    Add files via upload

---
+++ Lista-de-exercicios01/exercicio07.js
@@ -0,0 +1,17 @@
+/*7. As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, e R$ 0,25 se
+forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs
+compradas, calcule e escreva o valor total da compra.
+*/
+
+let quantidadeMacas = 15 ;
+let precoMenorQueUmaDuzia = 0.30;
+let precoMaiorQueUmaDuzia = 0.25;
+let total;
+
+if ( quantidadeMacas < 12) {
+    total = quantidadeMacas * precoMenorQueUmaDuzia
+    console.log(`O valor total da compra é R$ ${total}.`)
+} else { 
+    total = quantidadeMacas * precoMaiorQueUmaDuzia
+    console.log(`O valor total da compra é R$ ${total}.`)
+}
