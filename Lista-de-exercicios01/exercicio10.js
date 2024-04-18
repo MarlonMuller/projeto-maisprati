be9c844924d2b41ca8696a91153fc2a02d573934
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-04-18 10:13:57 -0300

    Add files via upload

---
+++ Lista-de-exercicios01/exercicio10.js
@@ -0,0 +1,11 @@
+/*
+10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+*/
+
+// falta o prompt
+let numero = 5;
+let i;
+
+for (i = 0; i < 10; i++ ){
+    console.log(numero);
+}
