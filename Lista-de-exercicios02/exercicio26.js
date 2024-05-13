15907893d6a4c5aaf0f044e1acb6b8036aca1d7b
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-05-13 14:49:33 -0300

    Update exercicio26.js

--- Lista-de-exercicios02/exercicio26.js
+++ Lista-de-exercicios02/exercicio26.js
@@ -1,6 +1,41 @@
-/*
-26. Dadas duas matrizes numéricas A[1..3,1..5] e B[1..3,1..5], calcular a matriz produto P[1..3,1..5].
-*/
+/* 26. Dadas duas matrizes numéricas A[1..3,1..5] e B[1..3,1..5], calcular a matriz produto
+P[1..3,1..5].*/
+
+let A = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15]
+];
+
+let B = [
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25],
+    [26, 27, 28, 29, 30]
+];
+
+function calcularMatrizProduto(A, B) {
+    let linhas = A.length;
+    let colunas = B[0].length;
+
+    let P = new Array(linhas);
+    for (let i = 0; i < linhas; i++) {
+        P[i] = new Array(colunas).fill(0);
+    }
+
+    for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+            for (let k = 0; k < B.length; k++) { // Iterando sobre o número correto de colunas de B
+                P[i][j] += A[i][k] * B[k][j]; // Multiplicação correta das matrizes
+            }
+        }
+    }
+
+    return P;
+}
+
+let matrizProduto = calcularMatrizProduto(A, B);
+
+console.log(matrizProduto);
 
 
 
