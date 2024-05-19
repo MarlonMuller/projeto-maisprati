f77535c344de575bfc97648bf164112310f5a5c6
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-05-19 15:14:01 -0300

    update19.05.24-2

---
+++ Lista-de-exercicios02/exercicio40.js
@@ -0,0 +1,37 @@
+/*
+40. Faça um algoritmo que leia um vetor de 5 elementos inteiros correspondentes ao resultado oficial da Loto. A seguir, leia 50 conjuntos de vetores (com 5 elementos inteiros cada), representando as apostas feitas. Compare os números das apostas com o resultado oficial e mostre uma mensagem ("Ganhador") se todos os números corresponderem ao resultado oficial. (Observação: não é necessário procurar por ternos e quadras, apenas por quinas.)
+*/
+
+function main() {
+
+    const resultadoOficial = [3, 15, 22, 30, 45]
+
+    const apostas = [
+        [3, 15, 22, 30, 45], [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40], [41, 42, 43, 44, 45],
+        [3, 15, 22, 30, 45], [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40], [41, 42, 43, 44, 45],
+        [3, 15, 22, 30, 45], [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40], [41, 42, 43, 44, 45],
+        [3, 15, 22, 30, 45], [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40], [41, 42, 43, 44, 45],
+        [3, 15, 22, 30, 45], [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25], [26, 27, 28, 29, 30], [31, 32, 33, 34, 35], [36, 37, 38, 39, 40], [41, 42, 43, 44, 45]
+    ];
+
+    function arraysIguais (arr1, arr2) {
+        if (arr1.length !== arr2.length) return false
+        for(let i = 0; i < arr1.length; i++) {
+            if(arr1[i] !== arr2[i]) return false
+        }
+        return true
+    }
+
+    for(let i = 0; i < apostas.length; i++) {
+        if(arraysIguais(apostas[i], resultadoOficial)) {
+            console.log('Ganhador')
+        }
+    }
+}
+
+main()
