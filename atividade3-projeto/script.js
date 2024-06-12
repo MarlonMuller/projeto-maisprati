f4f3c2aa1dccf619df1ded873dd1f5b4eb7e0f05
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-06-11 21:34:24 -0300

    I finished the javascript

--- atividade3-projeto/script.js
+++ atividade3-projeto/script.js
@@ -1,3 +1,17 @@
-function adicionarAoCarrinho() {
+let valorCarrinho = document.getElementById('valorCarrinho')
+let valorTotal = 0
+let valorUnidade = 10
+
+
+function adicionarAoCarrinho(id) {
     alert('Item adicionado ao carrinho')
+    
+    let qtdInput = document.getElementById(`qtd${id}`).value
+
+    let quantidade = parseInt(qtdInput)
+    
+    
+    valorTotal += valorUnidade * quantidade
+
+    valorCarrinho.innerHTML = valorTotal
 }
