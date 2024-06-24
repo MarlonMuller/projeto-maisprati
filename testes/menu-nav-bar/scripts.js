d48d4fb36b8e62ecc8286b20be3046b43966fbcf
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-06-24 20:24:59 -0300

    update24.06.24

--- testes/menu-nav-bar/scripts.js
+++ testes/menu-nav-bar/scripts.js
@@ -1,9 +1,9 @@
-function classToggle () {
+function classToggle() {
     const navs = document.querySelectorAll('.navbarItems')
 
     navs.forEach(nav => nav.classList.toggle('navbarToggleShow'))
 
-}
     
+}
 
-document.querySelector('.navbarLinkToggle').addEventListener('click', classToggle)
+document.querySelector('.navbarLinkToggle').addEventListener('click',classToggle)
