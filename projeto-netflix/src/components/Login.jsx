8e95c7ed46921039c3c7a102eadce3b0dab6f252
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-08-06 21:30:09 -0300

    update06.08.24-2

--- projeto-netflix/src/components/Login.jsx
+++ projeto-netflix/src/components/Login.jsx
@@ -1,21 +1,23 @@
 import './Login.css'
 
-function Login () {
-    return(
+function Login() {
+    return (
         <div className='login-pai'>
             <div className="login-container">
                 <h1>Entrar</h1>
                 <form>
                     <input type="text" placeholder='Email ou número de celular' />
-                    <input type="password" placeholder='Senha'  />
-                    <button>Entrar</button>
+                    <input type="password" placeholder='Senha' />
+                    <button className='buttonOne'>Entrar</button>
+                    <p>OU</p>
+                    <button className='buttonTwo'>Use um código de login</button>
                 </form>
                 <div className="login-option">
+                    <a href="#">Esqueceu sua senha?</a>
                     <div>
                         <input type="checkbox" id='remember' />
                         <label htmlFor="remeber">Lembre-se de mim</label>
                     </div>
-                    <div><a href="#">Esqueceu a senha?</a></div>
                 </div>
                 <div className='login-signup'>
                     <p>Novo por aqui? <a href="#">Assine agora</a> </p>
