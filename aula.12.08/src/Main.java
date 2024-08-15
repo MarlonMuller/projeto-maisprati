e7edf13e6fed98ee32c5329e9acdcb37af25a3b9
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-08-15 20:37:41 -0300

    update15.08.24

--- aula.12.08/src/Main.java
+++ aula.12.08/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        StackExample stack = new StackExample();
+
+        stack.push(10);
+        stack.push(100);
+        stack.push(90);
+        System.out.println("Elemento no topo: " + stack.peek());
+        System.out.println("Elemento removido: " + stack.pop());
     }
-}
+} 
