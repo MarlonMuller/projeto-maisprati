2da7657676cdca3b88d0c29e05c49152e61d990c
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-08-17 20:35:55 -0300

    update17.08.24-2

--- aula41/netflixMovies/src/App.jsx
+++ aula41/netflixMovies/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
 import FeaturedMovie from './components/FeaturedMovie';
-
+import Header from './components/Header'
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
-  
   useEffect(() => {
     const loadAll = async () => {
       // Pega a lista total
@@ -19,8 +19,8 @@ export default () => {
       console.log(list);
 
       // Pega o Featured
-      let originals = list.filter(i=>i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random()* (originals[0].items.results.length -1))
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
@@ -35,19 +35,47 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
 
   return (
     <div className='page'>
 
-      {featuredData && 
-      <FeaturedMovie item={featuredData} />
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
       }
 
       <section className='lists' >
-      {movieList.map((item, key)=>(
-        <MovieRow key={key} title={item.title} items={item.items} />
-      ))}
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
       </section>
+
+      <footer>
+        Atividade 5 - Marlon Muller <br />
+        Dados TMDB
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className='loading'>
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
     </div>
   )
 }
