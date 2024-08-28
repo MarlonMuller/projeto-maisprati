379d777e140773480bf3760b59d9d4b9efe53822
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-08-28 10:11:02 -0300

    Update Principal.jsx

--- projeto-netflix/src/components/Principal.jsx
+++ projeto-netflix/src/components/Principal.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // import './Principal.css'
-import Tmdb from '../Tmdb'
-import MovieRow from '../components/MovieRow'
-import FeaturedMovie from '../components/FeaturedMovie';
-import HeaderPrincipal from '../components/HeaderPrincipal'
+import Tmdb from "../Tmdb";
+import MovieRow from "../components/MovieRow";
+import FeaturedMovie from "../components/FeaturedMovie";
+import HeaderPrincipal from "../components/HeaderPrincipal";
 
 export default () => {
-
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
@@ -18,15 +17,18 @@ export default () => {
       setMovieList(list);
       console.log(list);
 
-      // Pega o Featured
-      let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      // Originais netflix (Pega o Featured)
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let chosen = originals[0].items.results[randomChosen];
-      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
       setFeaturedData(chosenInfo);
       console.log(chosen);
       console.log(chosenInfo);
-    }
+    };
 
     loadAll();
   }, []);
@@ -38,35 +40,35 @@ export default () => {
       } else {
         setBlackHeader(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', scrollListener);
+    window.addEventListener("scroll", scrollListener);
 
     return () => {
-      window.removeEventListener('scroll', scrollListener);
-    }
+      window.removeEventListener("scroll", scrollListener);
+    };
   }, []);
 
   return (
-    <div className='page'>
-
+    <div className="page">
       <HeaderPrincipal black={blackHeader} />
 
-      {featuredData &&
-        <FeaturedMovie item={featuredData} />
-      }
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
-      <section className='lists' >
+      <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
 
-      {movieList.length <= 0 &&
-        <div className='loading'>
-            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          />
         </div>
-      }
+      )}
     </div>
-  )
-}
+  );
+};
