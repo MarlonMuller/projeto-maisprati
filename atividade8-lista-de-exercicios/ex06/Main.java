c33044879f87fa99005758de2483f0074abc42bf
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-09-12 10:44:50 -0300

    Add files via upload

---
+++ atividade8-lista-de-exercicios/ex06/Main.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+
+    List<Animal> animais = new ArrayList<>();
+
+    animais.add(new Cachorro());
+    animais.add(new Gato());
+    animais.add(new Vaca());
+
+    for(Animal animal : animais){
+      animal.emitirSom();
+    }
+  }
+}
